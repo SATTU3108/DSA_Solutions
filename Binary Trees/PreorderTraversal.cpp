928ef0418a7d18c07b95333e74359ca7bf19f909
SATTU3108 <127048911+SATTU3108@users.noreply.github.com>
2024-10-17 12:29:33 +0530

    Update PreorderTraversal.cpp

--- Binary Trees/PreorderTraversal.cpp
+++ Binary Trees/PreorderTraversal.cpp
@@ -12,6 +12,7 @@ struct Node {
     }
 };
 
+//Recursive solution
 void preorderHelper(Node *root,vector <int> &ans) {
     if (root!=NULL) {
         ans.push_back(root->data);
@@ -26,3 +27,30 @@ vector <int> preOrderTraversal(Node *root) {
 }
 //Time complexity:O(N)
 //Space complexity:O(N)
+
+
+//Iterative solution:
+/*Algorithm:
+    1. create an empty stack and push the root in it
+    2. make iterations until the stack is not empty
+        i. pop the top item of the stack
+        ii. push the right child of the removed node, if it exists and then the left child
+        iii. this is done in order to preprocess the left node first and then the right node
+*/
+vector <int> preOrderTraversal(Node *root) {
+    vector <int> preorder;
+    if (root==NULL) return preorder;
+    stack <Node*> st;
+    st.push(root);
+    while (!st.empty()) {
+        Node *node=st.top();
+        st.pop();
+        preorder.push_back(root->data);
+        if (root->right!=NULL) st.push(root->right);
+        if (root->left!=NULL) st.push(root->left);
+    }
+}
+//Time complexity:o(N)
+//Space complexity:O(N)
+
+//Although the space complexity for both iterative and recursive solution is the same, iterative solution is more favoured due to the fact that in the case of skewed trees with large depth, recursion call stack might get exhausted and can cause stack overflow
