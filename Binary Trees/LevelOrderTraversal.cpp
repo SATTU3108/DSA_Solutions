3e65de62efa4ba14561863680bc4eda4c809268e
SATTU3108 <127048911+SATTU3108@users.noreply.github.com>
2024-10-20 10:15:57 +0530

    Update LevelOrderTraversal.cpp

--- Binary Trees/LevelOrderTraversal.cpp
+++ Binary Trees/LevelOrderTraversal.cpp
@@ -39,4 +39,26 @@ vector<vector<int>> levelOrderTraversal(Node *root) {
 //Time complecxity:O(N)
 //Space complexity:O(N)
 
-//If the question would be to print the levels in spiral order, then on the basis or eventh iteration, either push the level array into the ans array as it is or first reverse it and then push it
+//If the question would be to print the levels in spiral order(zigzag order), then on the basis or eventh iteration, either push the level array into the ans array as it is or first reverse it and then push it
+vector <vector<int>> spralTraversal(Node *root) {
+    vector<vector<int>> ans;
+    if (root==NULL) return ans;
+    queue <Node*> q;
+    bool flag=true; //when this is true, traverse left to right and when this is false, traverse right to left
+    q.push(root);
+    while (!q.empty()) {
+        int size=q.size();
+        vector <int> row(size);
+        for (int i=0;i<size;i++) {
+            Node *node=q.front();
+            q.pop();
+            int index=flag?i:(size-i-1);
+            row[index]=node->data;
+            if (node->left!=NULL) q.push(node->left);
+            if (node->right!=NULL) q.push(node->right);
+        }
+        flag=!flag;
+        ans.push_back(row);
+    }
+    return ans;
+}
